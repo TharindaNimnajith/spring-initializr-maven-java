382c0b4f530884bc8e8bb5e91bb3faa452aebbba
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-08 02:54:52 +0530

    init

---
+++ src/main/java/com/example/springinitializrmavenjava/SpringInitializrMavenJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinitializrmavenjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrMavenJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitializrMavenJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springinitializrmavenjava/SpringInitializrMavenJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinitializrmavenjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrMavenJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
